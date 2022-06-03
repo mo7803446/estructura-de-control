7baa15c5760f91e4ad7930734234298cb982a87e
mo7803446 <mo7803446@gmail.com>
2022-06-03 14:24:06 -0500

    actualizado 03 junio 2022

---
+++ for.py
@@ -0,0 +1,13 @@
+
+cadena="Fundamentos de programacion"
+contador=0
+tabla=4
+
+ # for contador in cadena:
+ #   print(contador)
+
+ # for contador in range(0,15):
+ #   print(contador)
+
+for contador in range(0,11):
+        print(f"{tabla} x {contador}={contador*tabla}")
